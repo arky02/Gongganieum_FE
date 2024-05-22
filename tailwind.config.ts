4d028f9c1b578f49e98c0a1c34043c22784000f9
Geon <geonulim@gmail.com>
2024-05-22 10:20:06 +0900

    chore: tailwind 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,17 @@
 import type { Config } from "tailwindcss";
 
+const createPxEntries = (size: number) => {
+  return {
+    0: "0",
+    ...Array.from(Array(size + 1)).reduce((accumulator, _, i) => {
+      return { ...accumulator, [`${i * 4}`]: `${(i * 4)}px` };
+    }),
+  };
+};
+
+const PX_ENTRIES = createPxEntries(500);
+
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,12 +19,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    spacing: PX_ENTRIES,
+    zIndex: {
+      base: "1",
+      nav: "2",
+      popup: "999",
+      floating: "1000",
     },
   },
   plugins: [],
