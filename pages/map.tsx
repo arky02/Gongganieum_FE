2077d3bbd3634f88b6be2afd3c99e6c5b6fd18d6
Geon <geonulim@gmail.com>
2024-05-22 14:53:09 +0900

    Feat: 네이버 지도 추가

---
+++ pages/map.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+const MapPage = () => {
+  const initMap = () => {
+    const map = new window.naver.maps.Map('map', {
+      // center: new naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 10,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+      logoControl: false,
+      tileSpare: 10,
+      tileTransition: false,
+    });
+  };
+
+  useEffect(() => {
+    initMap();
+  }, []);
+
+  return <div id='map' className='h-screen w-screen'></div>;
+};
+
+export default MapPage;
