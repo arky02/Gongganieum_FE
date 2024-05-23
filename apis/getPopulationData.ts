4a3e2cc7b3ba234646c5651089db210b8b6adec8
Matthew Song <songww1997@naver.com>
2024-05-23 10:16:44 +0900

    Feat: 인구 데이터 불러오는 함수 추가

---
+++ apis/getPopulationData.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const SECRET_KEY = process.env.SEOUL_PUBLIC_API_KEY;
+// 테스트 주소
+const URL = `http://openapi.seoul.go.kr:8088/${SECRET_KEY}/xml/citydata_ppltn/1/5/광화문·덕수궁`;
+
+export const getPopulationData = async () => {
+  const response = await axios.get(URL);
+  return response;
+};
