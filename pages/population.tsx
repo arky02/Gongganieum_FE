fd131bc92731bc0cba1c6a56e13b20c8b50b09f4
Matthew Song <songww1997@naver.com>
2024-05-23 11:21:46 +0900

    Feat: 디바운스 훅 추가 및 적용

--- pages/population.tsx
+++ pages/population.tsx
@@ -1,21 +1,25 @@
 // 지역 이름, 핫플 온도, 성별 비율, 인기있는 연령대, 연령대별 비율, 실시간 인구 혼잡도, 실시간 업데이트 시간, 장소 혼잡도 지표, 실시간 인구 지표
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import useDebounce from 'hooks/useDebounce';
 import { getPopulationData } from 'apis/getPopulationData';
 
 const Population = () => {
-  const [placeName, setPlaceName] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+  const debouncedSearchText = useDebounce(searchValue, 1000);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPlaceName(e.target.value);
+    setSearchValue(e.target.value);
   };
 
-  const response = getPopulationData('서울역');
-  console.log(response);
+  useEffect(() => {
+    const response = getPopulationData(searchValue);
+    console.log(response);
+  }, [debouncedSearchText]);
 
   return (
     <div>
       <input
-        value={placeName}
+        value={searchValue}
         onChange={handleChange}
         placeholder='검색할 장소를 작성해주세요'
       />
