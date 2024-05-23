2f4966bcaba2d6f91aa1fa015c8f5ab71e870303
Matthew Song <songww1997@naver.com>
2024-05-23 10:29:12 +0900

    Fix: Next.js 환경변수 수정

--- apis/getPopulationData.ts
+++ apis/getPopulationData.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SECRET_KEY = process.env.SEOUL_PUBLIC_API_KEY;
+const SECRET_KEY = process.env.NEXT_PUBLIC_SEOUL_PUBLIC_API_KEY;
 // 테스트 주소
 const URL = `http://openapi.seoul.go.kr:8088/${SECRET_KEY}/xml/citydata_ppltn/1/5/광화문·덕수궁`;
 
