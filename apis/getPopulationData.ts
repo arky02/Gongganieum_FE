4b4c59e2caf7f4be7761049daf360ff0dd5e2a86
Matthew Song <songww1997@naver.com>
2024-05-23 10:56:38 +0900

    Fix: 인구 데이터 호출 함수 인자 추가

--- apis/getPopulationData.ts
+++ apis/getPopulationData.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 const SECRET_KEY = process.env.NEXT_PUBLIC_SEOUL_PUBLIC_API_KEY;
-// 테스트 주소
-const URL = `http://openapi.seoul.go.kr:8088/${SECRET_KEY}/xml/citydata_ppltn/1/5/광화문·덕수궁`;
 
-export const getPopulationData = async () => {
+export const getPopulationData = async (
+  areaName: string,
+  startIndex: number = 1,
+  endIndex: number = 5,
+) => {
+  const URL = `http://openapi.seoul.go.kr:8088/${SECRET_KEY}/xml/citydata_ppltn/${startIndex}/${endIndex}/${areaName}`;
   const response = await axios.get(URL);
   return response;
 };
