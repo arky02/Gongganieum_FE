1256532546fff0b1d543ee5ad5fdd3470466be9d
Matthew Song <songww1997@naver.com>
2024-05-24 15:42:44 +0900

    Design: 데이터 시각화 UI 추가

--- pages/population.tsx
+++ pages/population.tsx
@@ -34,6 +34,21 @@ const Population = () => {
     setPopulationData(response);
   };
 
+  let popularAge;
+  const getPopularAgeValue = () => {
+    const ageArray = [
+      population.ageTeenager,
+      population.ageTwenties,
+      population.ageThirties,
+      population.ageForties,
+      population.ageFifties,
+    ].map((el) => Number(el));
+
+    const maxValue = Math.max(...ageArray);
+    popularAge = (ageArray.indexOf(maxValue) + 1) * 10;
+    return popularAge;
+  };
+
   useEffect(() => {
     getData();
   }, [debouncedSearchText]);
@@ -49,26 +64,45 @@ const Population = () => {
     setPopulation({ ...populationObj });
   }, [populationData]);
 
-  console.log(population);
-
   return (
-    <div className='m-100 flex flex-col items-center'>
+    <div className='m-100 flex flex-col items-center gap-40'>
       <input
-        className='h-80 w-500 rounded-3xl bg-gray-300 p-40 text-2xl font-bold text-gray-600 placeholder:text-gray-400	'
+        className='mb-40 h-80 w-300 rounded-3xl bg-gray-300 p-32 text-xl font-bold text-gray-600	placeholder:text-gray-400'
         value={searchValue}
         onChange={handleChange}
         placeholder='검색할 장소를 작성해주세요'
       />
       {population.areaName && (
-        <div className='mt-60	 w-9/12'>
-          <h1 className='mb-12 text-3xl font-bold'>[{population.areaName}]</h1>
-          <p className='mb-12'>
-            <strong>{population.areaCongestionMessage}</strong> <br />
-            10대 비율: <strong>{population.ageTwenties}</strong> <br />
+        <div className='flex w-400 flex-col gap-16'>
+          <div className='flex items-center justify-between'>
+            <h1 className='mb-12 text-2xl font-bold'>{population.areaName}</h1>
+            <p className='text-xs'>
+              현재 상태:
+              <span className='text-rose-500'> {population.areaState}</span>
+            </p>
+          </div>
+          <p className='flex justify-end text-xs	text-gray-500'>
+            업데이트 시간: {population.updateTime}
+          </p>
+          <div className='text-pretty rounded-lg border-2 border-solid border-gray-300 p-12'>
+            <p>{population.areaCongestionMessage}</p>
+          </div>
+          <h2 className='text-l mb-12 font-bold'>
+            지금 &apos;{population.areaName}&apos;에는&nbsp;
+            <span className='text-orange-400'>
+              {getPopularAgeValue()}대,
+              {population.maleRate > population.femaleRate ? '남성' : '여성'}
+            </span>
+            이 많아요.
+          </h2>
+          <p>
+            10대 비율: <strong>{population.ageTeenager}</strong> <br />
             20대 비율: <strong>{population.ageTwenties}</strong> <br />
             30대 비율: <strong>{population.ageThirties}</strong> <br />
             40대 비율: <strong>{population.ageForties}</strong> <br />
             50대 비율: <strong>{population.ageFifties}</strong> <br />
+            남성 비율: <strong>{population.maleRate}</strong> <br />
+            여성 비율: <strong>{population.femaleRate}</strong> <br />
           </p>
         </div>
       )}
