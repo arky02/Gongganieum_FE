cdb0582576287dec44d72dded71a29cf7a0b2276
Matthew Song <songww1997@naver.com>
2024-05-24 10:55:36 +0900

    Refactor: 인구 데이터 가공 함수 리팩토링

--- utils/populationDataSpliter.ts
+++ utils/populationDataSpliter.ts
@@ -1,16 +1,10 @@
 import { populationDataFilter } from 'constants/populationDataFilter';
-
-type PopulationDataFilterKeysType = keyof typeof populationDataFilter;
+import { PopulationDataFilterKeysType } from 'types/client.types';
 
 export const populationDataSpliter = (
   populationData: string,
   category: PopulationDataFilterKeysType,
-  callback: (arg0: string) => void,
 ) => {
   const areaNameSplit = populationData.split(populationDataFilter[category][0]);
-  if (areaNameSplit.length > 1) {
-    callback(areaNameSplit[1].split(populationDataFilter[category][1])[0]);
-  } else {
-    callback('');
-  }
+  return areaNameSplit[1].split(populationDataFilter[category][1])[0];
 };
