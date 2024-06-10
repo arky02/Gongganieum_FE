8fb51d925a79c69de1fe202123a81aea98707f78
Geon <geonulim@gmail.com>
2024-06-10 10:40:55 +0900

    Fix: 검색 인풋 as 파라미터 분리

--- components/commons/SearchInput.tsx
+++ components/commons/SearchInput.tsx
@@ -1,21 +1,24 @@
-import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
+import { ChangeEvent, Dispatch, SetStateAction } from 'react';
 import Dropdown from './Dropdown';
-
-const DROPDOWN_MENU = ['지역명', '빌딩명', '팝업명'];
 
 interface Props {
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
+  dropdownMenu?: string[];
+  selectedMenu?: string;
+  setSelectedMenu?: Dispatch<SetStateAction<string>>;
 }
 
-const SearchInput = ({ value, setValue }: Props) => {
+const SearchInput = ({
+  value,
+  setValue,
+  dropdownMenu,
+  selectedMenu,
+  setSelectedMenu,
+}: Props) => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(String(e.target.value));
   };
-
-  const [as, setAs] = useState<(typeof DROPDOWN_MENU)[number]>(
-    DROPDOWN_MENU[0],
-  );
 
   return (
     <form className='flex h-52 w-full'>
@@ -24,7 +27,13 @@ const SearchInput = ({ value, setValue }: Props) => {
         onChange={handleChange}
         className='w-full border border-black'
       />
-      <Dropdown elements={DROPDOWN_MENU} selected={as} setSelected={setAs} />
+      {dropdownMenu && selectedMenu && setSelectedMenu && (
+        <Dropdown
+          elements={dropdownMenu}
+          selected={selectedMenu}
+          setSelected={setSelectedMenu}
+        />
+      )}
       <button className='w-52 border border-black'>검색</button>
     </form>
   );
---
+++ constants/dropdown.ts
@@ -0,0 +1,1 @@
+export const SEARCH_AS = ['지역명', '빌딩명', '팝업명'];
