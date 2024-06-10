6e947983cec77148cece61db3aee3c745df64fd0
Matthew Song <songww1997@naver.com>
2024-06-10 10:31:37 +0900

    Fix: 버튼 및 색깔 로직 수정

--- components/pages/home/Slider.tsx
+++ components/pages/home/Slider.tsx
@@ -1,35 +1,75 @@
+import { useRef } from 'react';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 import { Autoplay, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+const Style = [
+  'bg-blue-100',
+  'bg-blue-200',
+  'bg-blue-300',
+  'bg-blue-400',
+  'bg-blue-500',
+];
+
 const Slider = () => {
   const slides = Array.from({ length: 5 }).map(
     (el, index) => `Slide ${index + 1}`,
   );
+
+  const swiperRef = useRef(null);
+
+  const handlePrev = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  const handleNext = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
   return (
-    <div className='flex items-center justify-center'>
+    <div className='relative m-40 flex items-center justify-center'>
       <Swiper
+        ref={swiperRef}
         modules={[Navigation, Autoplay]}
-        navigation
+        navigation={{ nextEl: null, prevEl: null }}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
-        spaceBetween={50}
+        loop={true}
+        spaceBetween={30}
         slidesPerView={1}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
-        className='h-600 w-600'
+        className='h-600 w-1000'
       >
         {slides.map((slideContent, index) => (
           <SwiperSlide
             key={slideContent}
             virtualIndex={index}
-            className={`bg-blue-${(index + 1) * 100}`}
+            className={`${Style[index]}`}
           >
             {slideContent}
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className='absolute -bottom-80 flex gap-8'>
+        <button
+          className='rounded-lg bg-gray-400 p-8 text-white'
+          onClick={handlePrev}
+        >
+          Prev
+        </button>
+        <button
+          className='rounded-lg bg-gray-400 p-8 text-white'
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
