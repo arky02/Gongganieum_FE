cb5a5abe377fbdaa7c5c61389331e25219468d13
Matthew Song <songww1997@naver.com>
2024-06-10 15:30:48 +0900

    Feat: 프로필 미리보기 기능 추가

--- components/pages/mypage/Profile.tsx
+++ components/pages/mypage/Profile.tsx
@@ -1,11 +1,54 @@
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+
+const defaultProfileImage =
+  'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
+
+const CIRCLE_RADIUS = 152;
+
 const Profile = () => {
+  const [image, setImage] = useState('');
+  const fileInput = useRef<HTMLInputElement>(null);
+
+  const handelClickImg = () => {
+    if (fileInput.current) fileInput.current.click();
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setImage(reader.result as string);
+        }
+      };
+    }
+  };
+
   return (
     <div className='flex items-center justify-center gap-40'>
       <div>
+        <div className={`w-${CIRCLE_RADIUS} h-${CIRCLE_RADIUS}`}>
+          <Image
+            width={CIRCLE_RADIUS}
+            height={CIRCLE_RADIUS}
+            src={image || defaultProfileImage}
+            alt='프로필 사진'
+            onClick={handelClickImg}
+            className='cursor-pointer rounded-full'
+          />
+        </div>
         <input
+          ref={fileInput}
           type='file'
           accept='image/jpg,impge/png,image/jpeg'
           name='profile_img'
+          onChange={onChange}
           className='hidden'
         />
       </div>
@@ -13,7 +56,7 @@ const Profile = () => {
         <div className='flex items-center gap-40'>
           <div className='text-2xl font-bold'>송민혁</div>
           <button className='rounded-lg bg-gray-100 p-8 hover:bg-gray-200'>
-            프로필 편집 버튼
+            프로필 편집
           </button>
         </div>
         <div className='h-100 w-500 rounded-lg bg-gray-100 p-16'>
