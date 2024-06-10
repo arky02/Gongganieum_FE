209b3cb641dec6da840856257ac198ae598f6651
Geon <geonulim@gmail.com>
2024-06-10 10:20:43 +0900

    Feat: 드롭다운 외부 눌렀을 때 닫히도록 구현

--- components/commons/Dropdown.tsx
+++ components/commons/Dropdown.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useRef } from 'react';
+import useOutsideClick from 'hooks/useOutsideClick';
 
 interface Props {
   elements: string[];
@@ -7,30 +8,28 @@ interface Props {
 }
 
 const Dropdown = ({ elements, selected, setSelected }: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useOutsideClick(ref, false);
 
-  const openMenu = () => {
-    setIsOpen(true);
-  };
-
-  const closeMenu = () => {
-    setIsOpen(false);
+  const handleMenuClick = (e: string) => {
+    setSelected(e);
+    setIsVisible(false);
   };
 
   return (
-    <div className='relative'>
+    <div ref={ref} className='relative'>
       <button
-        onClick={() => setIsOpen((prev) => !prev)}
+        onClick={() => setIsVisible((prev) => !prev)}
         type='button'
         className='h-full w-72 border border-black'
       >
         {selected}
       </button>
-      {isOpen && (
+      {isVisible && (
         <ul className='absolute'>
           {elements.map((e) => (
             <li
-              onClick={() => setSelected(e)}
+              onClick={() => handleMenuClick(e)}
               key={e}
               className='h-28 w-100 border border-black'
             >
