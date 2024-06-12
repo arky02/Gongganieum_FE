9e0f7c09203233118e51dca25a51c7f7d21cb700
Geon <geonulim@gmail.com>
2024-06-12 14:34:50 +0900

    Feat: 검색 결과 클릭 시 해당 핀으로 지도 이동

--- components/pages/map/BuildingCard.tsx
+++ components/pages/map/BuildingCard.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+import { useStore } from 'store';
 import { BuildingType } from 'types/client.types';
 
 interface Props {
@@ -5,11 +7,34 @@ interface Props {
 }
 
 const BuildingCard = ({ building }: Props) => {
+  const router = useRouter();
+  const { map } = useStore((state) => ({
+    map: state.map,
+  }));
+
+  const handleClick = () => {
+    if (!map) {
+      return;
+    }
+
+    router.push({ query: { building: building._id } });
+
+    const coord = building.coord.split(', ');
+    const position = new window.kakao.maps.LatLng(coord[0], coord[1]);
+    const bound = new window.kakao.maps.LatLngBounds();
+    bound.extend(position);
+    map.panTo(bound);
+  };
+
   return (
-    <div className='h-100 border border-black p-12'>
+    <button
+      onClick={handleClick}
+      type='button'
+      className='h-100 border border-black p-12 text-left'
+    >
       <p className='text-lg font-bold'>{building?.name}</p>
       <p>{building?.address}</p>
-    </div>
+    </button>
   );
 };
 
