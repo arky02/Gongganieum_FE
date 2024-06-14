83d50260041c64e95f08ca302e9278f32f644680
Matthew Song <songww1997@naver.com>
2024-06-14 13:59:26 +0900

    Refactor: 프로필 컴포넌트 props 관련 코드 수정

--- components/pages/mypage/Profile.tsx
+++ components/pages/mypage/Profile.tsx
@@ -1,33 +1,39 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
-const defaultProfileImage =
+const DEFAULT_PROFILE_IMAGE =
   'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
 
 const CIRCLE_RADIUS = 152;
 
-const Profile = () => {
+const Profile = (props: {
+  profileImage?: string | StaticImageData;
+  nickname: string;
+  introduction?: string;
+}) => {
+  const { profileImage, nickname, introduction } = props;
   return (
     <div className='flex items-center justify-center gap-40'>
-      <div>
-        <div className={`w-${CIRCLE_RADIUS} h-${CIRCLE_RADIUS}`}>
-          <Image
-            width={CIRCLE_RADIUS}
-            height={CIRCLE_RADIUS}
-            src={defaultProfileImage}
-            alt='프로필 사진'
-            className='cursor-pointer rounded-full bg-cover'
-          />
-        </div>
+      <div
+        className={`flex items-center justify-center overflow-hidden rounded-full`}
+      >
+        <Image
+          width={CIRCLE_RADIUS}
+          height={CIRCLE_RADIUS}
+          src={profileImage || DEFAULT_PROFILE_IMAGE}
+          alt='프로필 사진'
+          className='rounded-full object-cover'
+        />
       </div>
       <div className='flex flex-col gap-32'>
         <div className='flex items-center gap-40'>
-          <div className='text-2xl font-bold'>송민혁</div>
+          <div className='text-2xl font-bold'>{nickname}</div>
+          {/* TODO: 프로필 편집 페이지로 이동 */}
           <button className='rounded-lg bg-gray-100 p-8 hover:bg-gray-200'>
             프로필 편집
           </button>
         </div>
         <div className='h-100 w-500 rounded-lg bg-gray-100 p-16'>
-          소개문구랍니다.
+          {introduction}
         </div>
       </div>
     </div>
