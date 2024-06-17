fe9eaf6ce257ecb48b70772bdd3dbbeb38a05612
Matthew Song <songww1997@naver.com>
2024-06-17 15:59:32 +0900

    Feat: 리액트 포탈 컴포넌트 추가

---
+++ components/commons/PortalModal.tsx
@@ -0,0 +1,23 @@
+import { ReactElement, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+const PortalModal = (props: { children: ReactElement }) => {
+  const { children } = props;
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  // CSR을 마친 후, window객체가 있을 때만 동작 (SSR 단계에서는 에러)
+  if (typeof window === 'undefined') return <></>;
+
+  return mounted ? (
+    createPortal(children, document.getElementById('modal-root') as HTMLElement)
+  ) : (
+    <></>
+  );
+};
+
+export default PortalModal;
