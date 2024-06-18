8685f168e2b96de40786caa6601a6bf1a24a8916
Matthew Song <songww1997@naver.com>
2024-06-18 11:10:24 +0900

    Feat: 프로필 모달에 Input 컴포넌트 적용

--- components/commons/Button.tsx
+++ components/commons/Button.tsx
@@ -1,9 +1,14 @@
 import { ReactNode } from 'react';
 
-const Button = (props: { children: ReactNode; onClick?: () => void }) => {
-  const { children, onClick } = props;
+const Button = (props: {
+  children: ReactNode;
+  onClick?: () => void;
+  type?: 'submit' | 'button';
+}) => {
+  const { children, onClick, type = 'button' } = props;
   return (
     <button
+      type={type}
       onClick={onClick}
       className='rounded-10 text-16 flex gap-8 bg-black px-200 py-12 text-white'
     >
--- components/modals/ProfileModal.tsx
+++ components/modals/ProfileModal.tsx
@@ -1,23 +1,89 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
 import Button from 'components/commons/Button';
+import Input from 'components/commons/Input';
+
+interface FormValues {
+  nickname: string;
+  companyName: string;
+  brandName: string;
+  productOrServiceName: string;
+  interests: string;
+  introduction: string;
+}
 
 const ProfileModal = () => {
+  const { control, handleSubmit } = useForm<FormValues>({
+    defaultValues: {
+      nickname: '',
+      companyName: '',
+      brandName: '',
+      productOrServiceName: '',
+      interests: '',
+      introduction: '',
+    },
+  });
+
+  //TODO: 바꿀 로직
+  const submitProfileSettings: SubmitHandler<FormValues> = (formData) => {
+    console.log(formData);
+  };
+
   return (
-    <div className='rounded-24 flex h-full w-600 flex-col border-2 border-solid p-24'>
+    <form
+      onSubmit={handleSubmit(submitProfileSettings)}
+      className='rounded-24 flex h-full w-600 flex-col border-2 border-solid p-24'
+    >
       <div className='text-24 font-800'>기본 프로필 설정</div>
       <div className='text-16 font-400 text-gray-400'>
         공간이음 서비스를 이용하기 전 기본 프로필을 설정해 주세요.
       </div>
       <div className='flex flex-col gap-16 px-8'>
-        <div>닉네임</div>
-        <div>회사명</div>
-        <div>브랜드명</div>
-        <div>주요 제품 및 서비스명</div>
-        <div>관심 분야</div>
-        <div>한줄 소개</div>
+        <Input
+          name='nickname'
+          placeholder='닉네임을 입력해 주세요.'
+          control={control}
+        >
+          닉네임
+        </Input>
+        <Input
+          name='companyName'
+          placeholder='회사명을 입력해 주세요.'
+          control={control}
+        >
+          회사명
+        </Input>
+        <Input
+          name='brandName'
+          placeholder='브랜드명을 입력해 주세요.'
+          control={control}
+        >
+          브랜드명
+        </Input>
+        <Input
+          name='productOrServiceName'
+          placeholder='주요 제품 및 서비스명을 입력해 주세요.'
+          control={control}
+        >
+          주요 제품 및 서비스명
+        </Input>
+        <Input
+          name='interests'
+          placeholder='관심분야를 입력해 주세요.'
+          control={control}
+        >
+          관심 분야
+        </Input>
+        <Input
+          name='introduction'
+          placeholder='한 줄 소개를 입력해 주세요.'
+          control={control}
+        >
+          한 줄 소개
+        </Input>
       </div>
       {/* TODO: onClick 로직 추가 */}
-      <Button>기본 프로필 설정</Button>
-    </div>
+      <Button type='submit'>기본 프로필 설정</Button>
+    </form>
   );
 };
 
