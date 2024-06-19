5fd8b7ee92e3fb5b5663f06cb83418c294aab1ca
Geon <geonulim@gmail.com>
2024-06-19 17:35:36 +0900

    Feat: 팝업 이력 카드 구현

--- components/commons/description/PopupHistoryCard.tsx
+++ components/commons/description/PopupHistoryCard.tsx
@@ -1,5 +1,31 @@
-const PopupHistoryCard = () => {
-  return;
+import { PopupType } from 'types/client.types';
+import DescriptionCard from './DescriptionCard';
+
+const PopupHistoryCard = (props: { popups: PopupType[] }) => {
+  const { popups } = props;
+
+  return (
+    <DescriptionCard title='팝업 이력'>
+      <ul className='flex h-232 w-full flex-col gap-8 overflow-auto'>
+        {popups.map((popup) => (
+          <Popup key={popup.name} popup={popup} />
+        ))}
+      </ul>
+    </DescriptionCard>
+  );
 };
 
 export default PopupHistoryCard;
+
+const Popup = (props: { popup: PopupType }) => {
+  const { popup } = props;
+  return (
+    <li className=''>
+      <div className='flex justify-between'>
+        <span>{popup.name}</span>
+        <span>{popup.type}</span>
+      </div>
+      <div>{popup.date}</div>
+    </li>
+  );
+};
--- components/commons/description/PopupInfo.tsx
+++ components/commons/description/PopupInfo.tsx
@@ -1,5 +1,6 @@
 import { PopupType } from 'types/client.types';
 import DescriptionCard from './DescriptionCard';
+import PopupHistoryCard from './PopupHistoryCard';
 import PopupRankingCard from './PopupRankingCard';
 
 const PopupInfo = (props: { popups: PopupType[] }) => {
@@ -10,7 +11,7 @@ const PopupInfo = (props: { popups: PopupType[] }) => {
       <h3 className='mb-16 text-24 font-800'>팝업 정보</h3>
       <div className='flex flex-col gap-24'>
         <PopupRankingCard popups={popups} />
-        <DescriptionCard title='팝업 이력'></DescriptionCard>
+        <PopupHistoryCard popups={popups} />
       </div>
     </div>
   );
