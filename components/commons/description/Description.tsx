5b8963b82b5934183609b1acf2ee70601502bae7
Geon <geonulim@gmail.com>
2024-06-24 09:59:13 +0900

    Feat: 실시간 성비 카드 구현

--- components/commons/description/Description.tsx
+++ components/commons/description/Description.tsx
@@ -3,6 +3,7 @@ import { getPopulationData } from 'apis/getPopulationData';
 import { PopupType } from 'types/client.types';
 import BuildingInfoCard from './BuildingInfoCard';
 import DescriptionCard from './DescriptionCard';
+import GenderRatioCard from './GenderRatioCard';
 import PopupHistoryCard from './PopupHistoryCard';
 import PopupRankingCard from './PopupRankingCard';
 
@@ -36,7 +37,10 @@ const Description = (props: {
       <div>
         <h3 className='mb-16 text-24 font-800'>XX시 지역 데이터</h3>
         <div className='flex flex-col gap-24'>
-          <DescriptionCard title='실시간 성비'></DescriptionCard>
+          <GenderRatioCard
+            male={Number(data?.maleRate) ?? 50}
+            female={Number(data?.femaleRate) ?? 50}
+          />
           <DescriptionCard title='연령대별 비율'></DescriptionCard>
           <DescriptionCard title='실시간 인구 및 혼잡도 추이 현황'></DescriptionCard>
           <DescriptionCard title='실시간 인구 구성 비율'></DescriptionCard>
