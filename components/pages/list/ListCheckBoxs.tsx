b1f29e9dec22a2def1b41a2eea6a5496eb8dd6f4
Matthew Song <songww1997@naver.com>
2024-07-01 15:45:16 +0900

    Fix: 새로고침해도 체크 상태 유지

--- components/pages/list/ListCheckBoxs.tsx
+++ components/pages/list/ListCheckBoxs.tsx
@@ -1,25 +1,45 @@
-// 새로고침해도 체크여부 유지 - query
+import useSearch from 'hooks/map/useSearch';
+
 const ListCheckBoxs = (props: {
   onClickIsPopup: () => void;
   onClickOurs: () => void;
 }) => {
   const { onClickIsPopup, onClickOurs } = props;
+  {
+    /* TODO: 진행중인 팝업 로직 추가 */
+  }
+  const { isours } = useSearch();
+
   return (
     <div className='flex gap-[10px] py-8'>
-      <CheckBoxInput text='진행중인 팝업' onChange={onClickIsPopup} />
-      <CheckBoxInput text='직영 건물' onChange={onClickOurs} />
+      {/* TODO: 진행중인 팝업 로직 추가 */}
+      <CheckBoxInput
+        text='진행중인 팝업'
+        onChange={onClickIsPopup}
+        isClicked={false}
+      />
+      <CheckBoxInput
+        text='직영 건물'
+        onChange={onClickOurs}
+        isClicked={isours}
+      />
     </div>
   );
 };
 
 export default ListCheckBoxs;
 
-const CheckBoxInput = (props: { text: string; onChange: () => void }) => {
-  const { text, onChange } = props;
+const CheckBoxInput = (props: {
+  text: string;
+  onChange: () => void;
+  isClicked: boolean;
+}) => {
+  const { text, onChange, isClicked } = props;
   return (
     <div className='flex items-center justify-center gap-8'>
       <input
         type='checkbox'
+        checked={isClicked}
         onChange={onChange}
         className='h-16	w-16 appearance-none rounded-4 border border-solid bg-gray-100 checked:border-0 checked:bg-[url("/icons/black-check.svg")] checked:bg-auto checked:bg-center checked:bg-no-repeat'
       />
