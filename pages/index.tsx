f154023cc3bd8f92113738df06f15e9513856331
arky02 <kyean07@naver.com>
2024-07-02 15:33:51 +0900

    Feat: OAuth 로그인/ 회원가입 로직 1차 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
+import { useSearchParams } from 'next/navigation';
+
 export default function Home() {
+  const params = useSearchParams();
+  const code = params.get('redirect');
+  console.log(code);
   return;
 }
--- pages/oauth/[provider].tsx
+++ pages/oauth/[provider].tsx
@@ -3,6 +3,8 @@ import { usePathname } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import useManageUserAccessToken from 'hooks/useManageAccessToken';
 
 const OAuthProvider = () => {
   const router = useRouter();
@@ -11,26 +13,56 @@ const OAuthProvider = () => {
   const code = params.get('code');
   console.log(provider);
   console.log(code);
-  let response = '';
+  const [accessToken, setAccessToken] = useState('');
+  const { saveUserAccessToken } = useManageUserAccessToken();
 
-  const reqmsg = async () => {
-    let response = '';
+  const getAccessTokenFromOAuth = async () => {
+    let oauthLoginRes: { accessToken: string; name: string; role: string };
     try {
-      response = await axios.get(
-        `https://gongganieum.shop/api/${provider}/callback?code=${code}`,
+      const { data: oauthLoginRes } = await axios.get(
+        `https://gongganieum.shop/api/oauth/callback?code=${code}&provider=${provider}`,
       );
 
-      console.log(response);
+      console.log(oauthLoginRes);
+      if (!oauthLoginRes) console.log('OAuth AccessToken Response Error');
+      // => OAuth 서버로부터 받은 Response 존재!
+
+      // cookie에 accessToken 정보 저장
+      await saveUserAccessToken({ data: oauthLoginRes?.accessToken });
+
+      redirectByUserRole({
+        role: oauthLoginRes.role,
+        name: oauthLoginRes.name,
+      });
     } catch {
       alert('서버 오류가 발생하였습니다. 잠시 후 다시 시도해 주세요.');
       return;
     }
-    return response;
+  };
+
+  const redirectByUserRole = ({
+    role,
+    name,
+  }: {
+    role: string;
+    name: string;
+  }) => {
+    // 1. ROLE: USER -> 이미 가입된 회원인 경우, /로 리다이렉트
+    if (role === 'USER') {
+      console.log('Oauth Login Type: role = User');
+      router.push('/');
+      toast.success(
+        `${name}님, ${provider === 'kakao' ? '카카오' : '네이버'}로 로그인 되었습니다!`,
+      );
+    }
+
+    // 2. ROLE: GUEST -> 회원가입 필요
+    console.log('Oauth Login Type: role = Guest');
+    router.push('/?redirect=signup');
   };
 
   useEffect(() => {
-    if (code) reqmsg();
-    console.log(response);
+    if (code) getAccessTokenFromOAuth();
   }, [code]);
 };
 
