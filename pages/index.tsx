f6e75ea11424b653e534aeb4c1a2d721246cce2a
Matthew Song <songww1997@naver.com>
2024-07-02 13:46:56 +0900

    Design: Home Banner 디자인 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
+import HomeBanner from 'components/pages/home/HomeBanner';
 import HomeSlider from 'components/pages/home/HomeSlider';
 
 export default function Home() {
   return (
-    <div className='flex items-center justify-center'>
-      {/* <Banner /> */}
-      <div className=''>
+    <div className='flex flex-col items-center justify-center gap-76'>
+      <div>
         <h1 className='mb-24 text-32 font-800'>이번 주 핫한 건물</h1>
+        <HomeSlider />
+      </div>
+      <HomeBanner />
+      <div>
+        <h1 className='mb-24 text-32 font-800'>서울 성동구 인기 건물</h1>
         <HomeSlider />
       </div>
     </div>
