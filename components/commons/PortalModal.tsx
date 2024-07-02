43113d0d6a4810d2a5b5d3da9e74ddca6a585b00
arky02 <kyean07@naver.com>
2024-07-02 16:40:56 +0900

    Feat: close modal when backdrop clicked

--- components/commons/PortalModal.tsx
+++ components/commons/PortalModal.tsx
@@ -1,8 +1,12 @@
 import { ReactElement, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
-const PortalModal = (props: { children: ReactElement }) => {
-  const { children } = props;
+const PortalModal = (props: {
+  children: ReactElement;
+  handleClick: () => void;
+  openStatus: boolean;
+}) => {
+  const { children, handleClick, openStatus } = props;
   const [mounted, setMounted] = useState<boolean>(false);
 
   useEffect(() => {
@@ -23,9 +27,13 @@ const PortalModal = (props: { children: ReactElement }) => {
   return (
     <>
       {mounted &&
+        openStatus &&
         createPortal(
           <div>
-            <div className='fixed left-0 top-0 z-floating size-full bg-[rgba(0,0,0,0.5)]' />
+            <div
+              className='fixed left-0 top-0 z-floating size-full bg-[rgba(0,0,0,0.5)]'
+              onClick={handleClick}
+            />
             <div className='gap-5 pb-58 shadow-main fixed left-1/2 top-1/2 z-floating flex h-fit w-fit -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-[15px] bg-white'>
               {children}
             </div>
