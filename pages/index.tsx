7fd8bba071fe163de6b85ac9384e39b4887d2ac3
Matthew Song <songww1997@naver.com>
2024-07-02 14:26:30 +0900

    Design: 에디터 추천 배너 디자인 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import HomeBanner from 'components/pages/home/HomeBanner';
+import HomeEditorRecommend from 'components/pages/home/HomeEditorRecommend';
 import HomeSlider from 'components/pages/home/HomeSlider';
 
 export default function Home() {
@@ -12,6 +13,10 @@ export default function Home() {
       <div>
         <h1 className='mb-24 text-32 font-800'>서울 성동구 인기 건물</h1>
         <HomeSlider />
+      </div>
+      <div className='flex w-full'>
+        <HomeEditorRecommend />
+        <HomeEditorRecommend />
       </div>
     </div>
   );
