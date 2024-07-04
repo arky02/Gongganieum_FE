a696f281cbc0be5c801e7635415dcd73b6896fd5
arky02 <kyean07@naver.com>
2024-07-04 17:05:58 +0900

    design: Modal BackDrop darker

--- components/commons/PortalModal.tsx
+++ components/commons/PortalModal.tsx
@@ -29,7 +29,7 @@ const PortalModal = (props: {
         openStatus &&
         createPortal(
           <div>
-            <div className='fixed left-0 top-0 z-floating size-full bg-[rgba(0,0,0,0.5)]' />
+            <div className='fixed left-0 top-0 z-floating size-full bg-[rgba(0,0,0,0.6)]' />
             <div className='gap-5 pb-58 shadow-main fixed left-1/2 top-1/2 z-floating flex h-fit w-fit -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-[15px] bg-white'>
               {children}
             </div>
