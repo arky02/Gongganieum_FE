b337205cd88abbaf42548afd6a839b453ca4a177
arky02 <kyean07@naver.com>
2024-07-08 10:48:08 +0900

    style: 주석 제거

--- apis/config/default.ts
+++ apis/config/default.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import { useCookies } from 'react-cookie';
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
