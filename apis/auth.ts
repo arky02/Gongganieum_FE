0a61d42543c95baa3f893e985b4a1af9237e6ce4
arky02 <kyean07@naver.com>
2024-07-08 10:47:56 +0900

    feat: axios intercepter 적용, cookie fetching 로직 삭제

--- apis/auth.ts
+++ apis/auth.ts
@@ -2,13 +2,10 @@ import { FormValues } from 'components/modals/ProfileModal';
 import { instance } from './config/default';
 
 // 회원가입
-export const postUserSignUpInfo = async ({
-  formData,
-  accessToken,
-}: {
+export const postUserSignUpInfo = async (props: {
   formData: FormValues & { interests: string };
-  accessToken: string;
 }) => {
+  const { formData } = props;
   const {
     nickname,
     companyName: company,
@@ -17,36 +14,18 @@ export const postUserSignUpInfo = async ({
     introduction: description,
   } = formData;
 
-  const response = await instance.patch(
-    `/user/guest/update`,
-    {
-      nickname,
-      company,
-      brand,
-      tag,
-      description,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
-  );
+  const response = await instance.patch(`/user/guest/update`, {
+    nickname,
+    company,
+    brand,
+    tag,
+    description,
+  });
   return { resStatus: response.status, resData: response.data };
 };
 
-export const requestUserRole = async (accessToken: string) => {
-  const userRoleRes = await instance.get(`/user/info/role`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  return userRoleRes?.data;
-};
-
-export const getUserRole = (accessToken: string) => {
-  return {
-    queryKey: ['userRole', accessToken],
-    queryFn: () => requestUserRole(accessToken),
-  };
+// 유저 ROLE 체크
+export const requestUserRole = async () => {
+  const userRoleRes = await instance.get(`/user/info/role`);
+  return userRoleRes?.data?.user_role;
 };
