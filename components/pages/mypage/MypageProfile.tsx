5a1938b5d276d6f4b3b96ebefa86f814a555a39c
Matthew Song <songww1997@naver.com>
2024-07-09 10:49:55 +0900

    Design: 찜하기 리스트 레이아웃 디자인 추가

--- components/pages/mypage/MypageProfile.tsx
+++ components/pages/mypage/MypageProfile.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 
 const DEFAULT_PROFILE_IMAGE = '/images/default-profile-image.png';
-const CIRCLE_RADIUS = 152;
 
 const MypageProfile = (props: {
   profileImage?: string;
@@ -12,7 +11,7 @@ const MypageProfile = (props: {
   const { profileImage, nickname, email, introduction } = props;
   return (
     // TODO: 데이터 넣기
-    <div className='my-76 flex w-full items-center justify-between px-16'>
+    <div className='mb-76 flex w-full items-center justify-between px-16'>
       <div className='flex items-center gap-24'>
         {/* 프로필 이미지 */}
         <div className='relative h-96 w-96 rounded-full'>
@@ -32,7 +31,6 @@ const MypageProfile = (props: {
           <span className='text-16 font-400'>{introduction}</span>
         </div>
       </div>
-
       {/* TODO: 프로필 편집 페이지로 이동 */}
       {/* 프로필 편집 버튼 */}
       <button className='rounded-10 border border-solid border-black bg-white px-20 py-12 text-[15px] font-700 hover:bg-gray-100'>
--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,15 +1,69 @@
+import ListBuildingCard from 'components/pages/list/ListBuildingCard';
 import MypageProfile from 'components/pages/mypage/MypageProfile';
 
 const Mypage = () => {
   return (
-    <div className='mx-auto flex max-w-1232 flex-col items-center'>
+    <div className='mx-auto my-76 flex max-w-1232 flex-col items-center'>
       <MypageProfile
         nickname='민혁'
         email='songww1997@naver.com'
-        introduction='처한 상황에 따라 변하는게 좋을 수 있다!
-'
+        introduction='처한 상황에 따라 변하는게 좋을 수 있다!'
       />
-      <div>내 찜 목록</div>
+      <div className='flex w-full flex-col gap-24 px-16'>
+        <h1 className='mb-24 text-32 font-800'>내 찜 목록</h1>
+        {/* 찜하기 카드 리스트 */}
+        <div className='grid grid-cols-3 gap-x-24 gap-y-48'>
+          {/* TODO: 카드컴포넌트 공용으로 빼기 */}
+          <ListBuildingCard
+            id={0}
+            name={'노송 오재'}
+            address={'전라도 전주시'}
+            isours={true}
+            cate={'뷰티'}
+            latest_end_date={''}
+          />
+          <ListBuildingCard
+            id={0}
+            name={'노송 오재'}
+            address={'전라도 전주시'}
+            isours={true}
+            cate={'뷰티'}
+            latest_end_date={''}
+          />
+          <ListBuildingCard
+            id={0}
+            name={'노송 오재'}
+            address={'전라도 전주시'}
+            isours={true}
+            cate={'뷰티'}
+            latest_end_date={''}
+          />
+          <ListBuildingCard
+            id={0}
+            name={'노송 오재'}
+            address={'전라도 전주시'}
+            isours={true}
+            cate={'뷰티'}
+            latest_end_date={''}
+          />
+          <ListBuildingCard
+            id={0}
+            name={'노송 오재'}
+            address={'전라도 전주시'}
+            isours={true}
+            cate={'뷰티'}
+            latest_end_date={''}
+          />
+          <ListBuildingCard
+            id={0}
+            name={'노송 오재'}
+            address={'전라도 전주시'}
+            isours={true}
+            cate={'뷰티'}
+            latest_end_date={''}
+          />
+        </div>
+      </div>
     </div>
   );
 };
