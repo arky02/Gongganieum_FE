6f0127594d7309ae0d3209fb9e755b4796b55d38
Matthew Song <songww1997@naver.com>
2024-07-10 10:33:41 +0900

    Feat: 마이페이지에 찜한 건물 리스트 가져오기

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,20 +1,29 @@
 import { useQuery } from '@tanstack/react-query';
-import { getLikeBuildings, getMyInfo } from 'apis/api';
+import { getBuildings, getLikeBuildingIds, getMyInfo } from 'apis/api';
 import { BuildingType } from 'types/client.types';
 import BuildingCard from 'components/commons/BuildingCard';
 import MypageProfile from 'components/pages/mypage/MypageProfile';
 
 const Mypage = () => {
-  const userId = 1; // test ID
+  const userId = 118; // test ID
 
   const { data: userInfo } = useQuery({
     queryKey: ['userInfo', userId],
     queryFn: () => getMyInfo(userId),
   });
 
-  const { data: likeBuildings }: { data?: BuildingType[] } = useQuery({
-    queryKey: ['likeBuilding', userId],
-    queryFn: () => getLikeBuildings(userId),
+  const { data: likeBuildingIds }: { data?: number[] } = useQuery({
+    queryKey: ['likeBuildingIds', userId],
+    queryFn: () => getLikeBuildingIds(userId),
+  });
+
+  const { data: buildings }: { data?: BuildingType[] } = useQuery({
+    queryKey: ['buildings'],
+    queryFn: () => getBuildings(),
+  });
+
+  const likeBuildings = buildings?.filter((el) => {
+    return likeBuildingIds?.includes(el._id);
   });
 
   return (
@@ -24,12 +33,11 @@ const Mypage = () => {
         email={userInfo?.email}
         introduction={userInfo?.description}
       />
+      {/* 찜하기 카드 리스트 */}
       <div className='flex w-full flex-col gap-24 px-16 md:my-28'>
         <h1 className='mb-24 text-32 font-800'>내 찜 목록</h1>
-        {/* 찜하기 카드 리스트 */}
         <div className='grid min-h-300 grid-cols-3 gap-x-24 gap-y-48 md:grid-cols-2 md:gap-x-8 md:gap-y-36'>
           {likeBuildings &&
-            likeBuildings.length > 0 &&
             likeBuildings?.map((building: BuildingType) => (
               <BuildingCard
                 mode='like'
@@ -42,28 +50,6 @@ const Mypage = () => {
                 latest_end_date={building.latest_end_date}
               />
             ))}
-          <BuildingCard
-            mode='like'
-            key={0}
-            _id={0}
-            name={'울집'}
-            address={'경기도 과천'}
-            isours={true}
-            cate={'패션'}
-            tag={'과천, 돈가스, 서울랜드'}
-            latest_end_date={''}
-          />
-          <BuildingCard
-            mode='like'
-            key={0}
-            _id={0}
-            name={'울집'}
-            address={'경기도 과천'}
-            isours={true}
-            cate={'패션'}
-            tag={'과천, 돈가스, 서울랜드'}
-            latest_end_date={''}
-          />
         </div>
       </div>
     </div>
