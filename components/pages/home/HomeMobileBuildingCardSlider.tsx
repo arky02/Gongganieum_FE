cee0d6370f1696e167304b53e53a7db02e69e680
Matthew Song <songww1997@naver.com>
2024-07-17 15:46:44 +0900

    Fix: any 타입 제거

--- components/pages/home/HomeMobileBuildingCardSlider.tsx
+++ components/pages/home/HomeMobileBuildingCardSlider.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { ScrollContainer } from 'react-indiana-drag-scroll';
 import 'react-indiana-drag-scroll/dist/style.css';
 import { getHomeCarousel } from 'apis/api';
+import { CarouselType } from 'types/client.types';
 import BuildingCard from 'components/commons/BuildingCard';
 
 const HomeMobileBuildingCardSlider = (props: {
@@ -25,16 +26,14 @@ const HomeMobileBuildingCardSlider = (props: {
     // scrollbar-hide 사용
     <div className='hidden w-[100dvw] whitespace-nowrap scrollbar-hide md:flex md:overflow-x-scroll'>
       <ScrollContainer className='md:flex md:gap-16 md:px-24'>
-        {carouselData?.map((building: any) => {
+        {carouselData?.map((building: CarouselType) => {
           return (
-            <div key={building} className='inline-block'>
-              <BuildingCard
-                mode='home'
-                key={building._id}
-                _id={building.contentId}
-                building={building.content}
-              />
-            </div>
+            <BuildingCard
+              mode='home'
+              key={building._id}
+              _id={building.contentId}
+              building={building.content}
+            />
           );
         })}
       </ScrollContainer>
