1eefebc59f2325dc7bf0d8a00c0ebb7dd3ccf6e1
Matthew Song <songww1997@naver.com>
2024-07-17 14:28:23 +0900

    Design: 홈페이지 크기 및 갭 수정

--- components/pages/home/HomeEditorRecommend.tsx
+++ components/pages/home/HomeEditorRecommend.tsx
@@ -3,26 +3,26 @@ import { IconLogo } from 'public/icons';
 
 const HomeEditorRecommend = () => {
   return (
-    <div className='flex h-full w-full items-center justify-center bg-gray-100 md:px-16 md:py-56'>
-      <div className='flex max-w-412 flex-col gap-36'>
-        <div className='w-120'>
+    <div className='flex h-full w-full items-center justify-center bg-gray-100 md:px-16 md:py-20'>
+      <div className='flex max-w-412 flex-col gap-36 md:gap-16'>
+        <div className='w-120 md:w-100'>
           <IconLogo />
         </div>
-        <div className='flex flex-col gap-8'>
-          <h1 className='text-32 font-800'>
-            2024 상반기 결산 <br />
-            공간이음 에디터 추천
+        <div className='flex flex-col gap-8 md:gap-4'>
+          <h1 className='flex flex-col text-32 font-800 md:text-28'>
+            <span>2024 상반기 결산</span>
+            <span>공간이음 에디터 추천</span>
           </h1>
           <span className='text-16 font-400'>
             공간이음 에디터가 선별한 인기 건물을 확인해 보세요!
           </span>
         </div>
-        <div className='flex justify-center'>
+        <div className='relative mx-auto h-296 w-296 md:h-240 md:w-240'>
           <Image
             src={'/images/home-editor-recommend-image.png'}
             alt='공간이음 에디터 추천 이미지'
-            width={296}
-            height={296}
+            fill
+            className='object-cover'
           />
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -33,16 +33,18 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='mb-76 mt-76 flex  flex-col items-center justify-center gap-76 md:mt-0 md:items-start'>
+    <div className='mb-76 mt-76 flex  flex-col items-center justify-center gap-76 md:mb-24 md:mt-0 md:items-start md:gap-24'>
       <HomeSliderWithPagination mode='main_banner' />
       <div className='md:mx-24'>
-        <h1 className='mb-24 text-32 font-800'>이번 주 핫한 건물</h1>
+        <h1 className='mb-24 text-32 font-800 md:mb-16'>이번 주 핫한 건물</h1>
         <HomeCardSlider mode='primary' />
         <HomeMobileBuildingCardSlider mode='primary' />
       </div>
       <HomeBanner />
       <div className='md:mx-24'>
-        <h1 className='mb-24 text-32 font-800'>서울 성동구 인기 건물</h1>
+        <h1 className='mb-24 text-32 font-800 md:mb-16'>
+          서울 성동구 인기 건물
+        </h1>
         <HomeCardSlider mode='secondary' />
         <HomeMobileBuildingCardSlider mode='secondary' />
       </div>
@@ -51,7 +53,7 @@ const Home = () => {
         <HomeSliderWithPagination mode='recommend_banner' />
       </div>
       <div className='md:mx-24'>
-        <h1 className='mb-24 text-32 font-800'>인기 매거진 소개</h1>
+        <h1 className='mb-24 text-32 font-800 md:mb-16'>인기 매거진 소개</h1>
         <HomeMagazineSlider />
         <HomeMobileMagazineSlider />
       </div>
