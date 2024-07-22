a00081e5e438fdf5692f64affb12662f2c5d409e
Matthew Song <songww1997@naver.com>
2024-07-22 11:15:41 +0900

    Design: 메거진 카테고리 추가

---
+++ pages/magazine.tsx
@@ -0,0 +1,19 @@
+import MagazineCategory from 'components/pages/magazine/MagazineCategory';
+
+const magazine = () => {
+  return (
+    <div className='my-48 flex flex-col items-center justify-center'>
+      {/* 카테고리 */}
+      <MagazineCategory />
+      {/* 메인 배너 */}
+
+      {/* 매거진 리스트 그리드 형태 */}
+
+      {/* 슬라이드 리스트 */}
+
+      {/* 피플 나우 리스트 */}
+    </div>
+  );
+};
+
+export default magazine;
