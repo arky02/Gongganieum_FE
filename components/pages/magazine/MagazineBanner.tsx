b7f098a0f1d3b5a5a5d3cbffae11a5a253931fd1
Matthew Song <songww1997@naver.com>
2024-07-22 15:06:19 +0900

    Design: 메거진 배너 추가

---
+++ components/pages/magazine/MagazineBanner.tsx
@@ -0,0 +1,21 @@
+// TODO: 멘트 수정, 이미지랑 데이터 연결
+const MagazineBanner = () => {
+  return (
+    <div className='relative  w-[90dvw] bg-[#000]'>
+      <div className="relative h-640 w-full bg-[url('/images/mock-son.webp')] bg-center bg-no-repeat">
+        <div className='flex h-full  w-full flex-col items-center justify-center gap-16 bg-[#000] opacity-60'></div>
+        <div className='absolute bottom-1/3 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-8'>
+          <div className='flex items-center justify-center gap-8 text-white opacity-60'>
+            <span className='border-r-2 pr-8 text-18 font-700'>패션 화보</span>
+            <span className='text-18 font-600 text-gray-200'>2024.07.21</span>
+          </div>
+          <h1 className='whitespace-nowrap text-[60px] font-800 text-white'>
+            송민혁과 보낸 가장 한국적인 휴가
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MagazineBanner;
--- pages/magazine.tsx
+++ pages/magazine.tsx
@@ -1,12 +1,13 @@
+import MagazineBanner from 'components/pages/magazine/MagazineBanner';
 import MagazineCategory from 'components/pages/magazine/MagazineCategory';
 
 const magazine = () => {
   return (
-    <div className='my-48 flex flex-col items-center justify-center'>
+    <div className='my-48 flex flex-col items-center justify-center gap-40'>
       {/* 카테고리 */}
       <MagazineCategory />
       {/* 메인 배너 */}
-
+      <MagazineBanner />
       {/* 매거진 리스트 그리드 형태 */}
 
       {/* 슬라이드 리스트 */}
