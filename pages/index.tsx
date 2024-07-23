13fb355655484e06db16467d489d34a5746525ba
Matthew Song <songww1997@naver.com>
2024-07-23 16:45:34 +0900

    Design: 메거진 메인 배너 반응형 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import HomeSliderWithPagination from 'components/pages/home/HomeSliderWithPagina
 
 const Home = () => {
   return (
-    <div className='mb-76 mt-76 flex  flex-col items-center justify-center gap-76 md:mb-24 md:mt-0 md:items-start md:gap-24'>
+    <div className='mb-76 mt-76 flex flex-col items-center justify-center gap-76 md:mb-24 md:mt-0 md:items-start md:gap-24'>
       <HomeSliderWithPagination mode='main_banner' />
       <div>
         <h1 className='mb-24 text-32 font-800 md:mx-24 md:mb-16 md:text-24'>
--- pages/magazine/index.tsx
+++ pages/magazine/index.tsx
@@ -7,18 +7,18 @@ import MagazineSlideList from 'components/pages/magazine/MagazineSlideList';
 
 const magazine = () => {
   return (
-    <div className='mb-48 mt-20 flex w-[100dvw] flex-col items-center justify-center gap-60'>
+    <div className='mb-48 mt-20 flex w-[100dvw] flex-col items-center justify-center gap-60 md:mb-0 md:mt-0 md:w-[calc(100%)]'>
       {/* 카테고리 */}
       {/* <MagazineCategory /> */}
       {/* 메인 배너 */}
       <MagazineMainBanner />
       {/* 매거진 리스트 그리드 형태 */}
-      <MagazineGridList />
+      {/* <MagazineGridList /> */}
       {/* 배너 및 슬라이드 리스트 */}
-      <MagazineBanner />
-      <MagazineSlideList />
+      {/* <MagazineBanner /> */}
+      {/* <MagazineSlideList /> */}
       {/* 피플 나우 리스트 */}
-      <MagazineListWithBackground />
+      {/* <MagazineListWithBackground /> */}
     </div>
   );
 };
