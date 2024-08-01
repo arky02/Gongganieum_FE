8390e9b43d3075292fe9ec99fc6188e8e3323b7d
Matthew Song <songww1997@naver.com>
2024-08-01 16:16:38 +0900

    Fix: 상세페이지에서 새 탭으로 가는 기능 삭제

--- components/commons/BuildingTitle.tsx
+++ components/commons/BuildingTitle.tsx
@@ -16,19 +16,26 @@ const BuildingTitle = (props: {
     <div
       className={`${page === 'map' ? 'mb-36 mt-12 md:mb-24 md:mt-16' : 'mb-56 md:mb-36'} flex w-full flex-col`}
     >
-      <Link href={`/list/${buildingInfo?._id}`} legacyBehavior passHref>
-        <a
-          target='_blank'
-          className={`${page == 'map' ? 'text-28' : 'text-[3.6rem]'} line-clamp-1 flex h-44 w-fit items-end gap-8 font-800 md:h-32 md:text-[2.2rem]`}
-        >
+      {page === 'map' ? (
+        <Link href={`/list/${buildingInfo?._id}`} legacyBehavior passHref>
+          <a
+            target='_blank'
+            className='line-clamp-1 flex h-44 w-fit items-end gap-8 text-28 font-800 md:h-32 md:text-[2.2rem]'
+          >
+            {buildingInfo?.name}
+            {page === 'map' && (
+              <div className='pb-[10px]'>
+                <IconArrowTopRight />
+              </div>
+            )}
+          </a>
+        </Link>
+      ) : (
+        <div className='line-clamp-1 flex h-44 w-fit items-end gap-8 text-[3.6rem] font-800 md:h-32 md:text-[2.2rem]'>
           {buildingInfo?.name}
-          {page === 'map' && (
-            <div className='pb-[10px]'>
-              <IconArrowTopRight />
-            </div>
-          )}
-        </a>
-      </Link>
+        </div>
+      )}
+
       <div className='mb-16 mt-8 line-clamp-1 flex h-24 items-center gap-4 text-16 font-500 text-gray-400 opacity-80 md:mb-12 md:mt-4 md:text-14'>
         <IconMarker />
         {buildingInfo?.address}
