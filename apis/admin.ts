cead8b17ea16137b011fdae764311c3351fb9587
arky02 <kyean07@naver.com>
2024-08-26 16:40:40 +0900

    Feat: 관리자 페이지에 필요한 api

--- apis/admin.ts
+++ apis/admin.ts
@@ -1,7 +1,8 @@
-import { AxiosError } from 'axios';
-import { postPopupType } from 'components/pages/admin/contents/CreatePopup';
+import { postCarouselType } from 'components/pages/admin/contents/CarouselCU';
+import { postPopupType } from 'components/pages/admin/contents/PopupCreate';
 import { instance } from './config/default';
 
+// DATA SAVE API
 // 새 빌딩 정보 추가
 export const postNewBuildingData = async (newBuildingFormData: FormData) => {
   const res = await instance.post('/admin/save/building', newBuildingFormData);
@@ -14,10 +15,65 @@ export const postNewPopupData = async (newPopupData: postPopupType) => {
   return res;
 };
 
+// 캐러셀 정보 추가
+export const postNewCarouselData = async (
+  newCarouselData: postCarouselType,
+) => {
+  const res = await instance.post(`/admin/save/carousel`, newCarouselData);
+  return res;
+};
+
+// DATA EDIT API
+// 기존 건물 정보 수정
+export const editBuildingData = async (buildingFormData: FormData) => {
+  const res = await instance.put('/admin/edit/building', buildingFormData);
+  return res;
+};
+
+// 기존 캐러셀 컨텐츠 정보 수정
+export const editCarouselData = async (
+  carouselData: postCarouselType & { id: number },
+) => {
+  const res = await instance.put(`/admin/edit/carousel`, carouselData);
+  return res;
+};
+
+// DATA DELETE API
+// 빌딩 정보 삭제
+export const deleteBuildingData = async (id: number) => {
+  const res = await instance.delete(`/admin/delete/building?id=${id}`);
+  return res;
+};
+
+// 케러셀 정보 삭제
+export const deleteCarouselData = async (id: number) => {
+  const res = await instance.delete(`/admin/delete/carousel?id=${id}`);
+  return res;
+};
+
+// 문의하기 정보 삭제
+export const deleteContactData = async (id: number) => {
+  const res = await instance.delete(`/admin/delete/contact?id=${id}`);
+  return res;
+};
+
+// 유저 정보 삭제
+export const deleteUserData = async (id: number) => {
+  const res = await instance.delete(`/admin/delete/user?id=${id}`);
+  return res;
+};
+
+// ADMIN AUTHORIZATION API
 // 관리자 비밀번호 검증
-export const authorizeAdmin = async (pwd: string) => {
+export const authorizeAdmin = async ({
+  pwd,
+  user,
+}: {
+  pwd: string;
+  user: string;
+}) => {
   try {
-    const res = await instance.post('/admin/authorize', { pwd });
+    const res = await instance.post('/admin/authorize', { pwd, user });
     return res.status;
   } catch (e: any) {
     return e.response.status;
